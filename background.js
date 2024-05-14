f1cd51e7c8bb73bc92f2d8984e20ae376d42054f
Eric Du <eric.du03@berkeley.edu>
2024-05-13 21:04:46 -0700

    migrated styling, optimized update function

--- background.js
+++ background.js
@@ -24,7 +24,6 @@ function validate(name, date) {
     return text;
 }
 
-// window.setInterval(getDateTime, 10000)
 function getDateTime() {
     let name = document.getElementById('name').value;
     let date = document.getElementById('time').value;
@@ -45,36 +44,20 @@ function addBar(name) {
     bar.high = 0.6;
     bar.optimum = 0.8;
     bar.value = 1;
-    bar.width = 300;
 
 
     // add bar to page
     let text = document.createElement("div");
-    let textId = deadlines.length; // ID'd based on the order in which they are due
+    let textId = deadlines.length - 1; // ID'd based on the order in which they are due, zero indexed
     text.innerHTML = name;
-
-    // TODO: Can I move this to a separate file 
-    {
-    text.style.fontSize = '15pt';
-    text.style.marginBottom='10px'
+    text.className = "textStyle";
     text.id = textId;
-    text.style.paddingLeft = "0.5cm";
-    text.style.paddingTop = "0.5cm";
-    text.style.paddingBottom = "0.5cm";
-    }
-
-    { // border stylings
-    text.style.border = "1px solid black";
-    text.style.borderRadius = "0.2cm";
-    text.style.borderLeft = "solid #32a1ce";
-    text.style.left = "10px";
-    // text.style.position = "";
-    }
     
     // create text showing deadline
     let afterText = document.createElement("div");
     let timestamp = new Date(deadlines[deadlines.length-1] - 1) // subtract 1 so it shows 11:59PM of the previous day
-    afterText.innerHTML = timestamp.toLocaleString('en-US', {month: "long", day: "2-digit", minute: "2-digit" , hour12:true, hour:"numeric"});
+    let time_fmt = {month: "long",day: "2-digit", minute: "2-digit", hour12:true, hour:"numeric"};
+    afterText.innerHTML = timestamp.toLocaleString('en-US', time_fmt);
     afterText.style.fontSize = '11pt';
 
     // add elements to HTML, with padding
@@ -84,38 +67,12 @@ function addBar(name) {
 
 }
 
-// window.setInterval(updateBar, 5000); // rate at which progress bars update
+window.setInterval(updateBar, 5000); // rate at which progress bars update
 
 function updateBar() { // method to update all progress bars periodically
     var now = Date.now();
-    document.getElementById("bars").innerHTML = ' ';
-
     for (let i = 0; i < deadlines.length; i++) {
-        let bar = document.createElement("meter");
-        bar.min = 0;
-        bar.max = 1;
-        bar.low = 0.2;
-        bar.high = 0.6;
-        bar.optimum = 0.8;
-
-        // Update bar progress
-        bar.value = (deadlines[i] - now)/(deadlines[i] - date_created[i]);
-
-        let text = document.createElement("p");
-        text.innerHTML = names[i];
-        text.style.fontSize = '15pt';
-        document.getElementById("bars").append(text);
-        document.getElementById("bars").append(bar);
-
-        let afterText = document.createElement("p");
-
-        let timestamp = new Date(deadlines[i] - 1) // subtract 1 so it shows 11:59PM of the previous day
-        afterText.innerHTML = timestamp.toLocaleString('en-US', {month: "long", day: "2-digit", minute: "2-digit" , hour12:true, hour:"numeric"});
-        bar.after(afterText);
-
-        let newline = document.createElement("body");
-        newline.style.paddingBottom = "10px"
-        document.getElementById("bars").append(newline);
+        document.getElementById(i).setAttribute("value", (deadlines[i] - now)/(deadlines[i]- date_created[i]));
     }
 }
 
